1c4f59bdb6a1f58f26dc467b7058d8c158d6bab0
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-11-26 13:53:56 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    const newDrivers = drivers.slice(0, 2);
+    return newDrivers;
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    const newDrivers = drivers.slice(2, 4);
+    return newDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => function () {
+    return multiplier * multiplier
+}
+
+const fareDoubler = fare => fare * 2;
+
+const fareTripler = fare => fare * 3;
+
+const selectDifferentDrivers = (arrayOfDrivers, functionSelector) => functionSelector(arrayOfDrivers);
